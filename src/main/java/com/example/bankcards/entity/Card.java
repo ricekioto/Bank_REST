c570bc3d035dc583863bb500efe181302b26d750
mmkit <ataichukuev3@gmail.com>
2025-08-08 10:34:56 +0500

    add entity

--- src/main/java/com/example/bankcards/entity/Card.java
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -1,5 +1,32 @@
 package com.example.bankcards.entity;
 
+import com.example.bankcards.entity.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.Id;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(nullable = false, length = 16)
+    Long number;
+
+    @Column(nullable = false)
+    String owner;
+
+    @Enumerated(EnumType.STRING)
+    Status status;
+
+    @Column(nullable = false, columnDefinition = "bigint default 0")
+    private Long balance;
 }
